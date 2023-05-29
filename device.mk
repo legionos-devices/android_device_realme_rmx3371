86c363ff9b4c567053a90174eaa80fcfc3d23249
Vaisakh Murali <mvaisakh@statixos.com>
2023-05-29 22:42:26 +0530

    rmx3371: inherit power-libperfmgr product
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- device.mk
+++ device.mk
@@ -347,10 +347,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+$(call inherit-product, hardware/oplus/aidl/power-libperfmgr/power_product.mk)
 
 # QMI
 PRODUCT_PACKAGES += \
