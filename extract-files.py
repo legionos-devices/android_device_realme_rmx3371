d6c7d98b8b025cd5e189a8ea90f647c101307959
Bruno Martins <bgcngm@gmail.com>
2024-09-07 22:47:03 +0100

    Patch libAlgoProcess to resolve duplicate dependencies
    Fixes:
    error: module "libAlgoProcess" variant
    "android_vendor_arm64_armv9-a_static": depends on multiple
    versions of the same aidl_interface:
    android.hardware.graphics.common-V3-ndk-source,
    android.hardware.graphics.common-V5-ndk-source
    
    Change-Id: If0be3da5c13e45c6c59dde98bb80f35eb22bf846
    Signed-off-by: projectlegionos <projectlegionos@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -120,6 +120,8 @@ blob_fixups: blob_fixups_user_type = {
          .add_needed('libcamera_metadata_shim.so'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V5-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
