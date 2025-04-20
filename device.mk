cbf29ff06a6e3bf2092aa30be42cac51a52661ee
rajkale99 <kaleraj.rk@gmail.com>
2025-04-20 20:00:19 +0530

    rmx3371:Build some power dependencies
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- device.mk
+++ device.mk
@@ -279,7 +279,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
